665472dfc1df09e89ac1b29af0d763115840fbfb
amanyadav84 <amanyadav843233@gmail.com>
2026-04-08 11:52:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,6 @@ def predict():
     except Exception as e:
         return render_template("index.html", prediction_text=f"Error: {str(e)}")
     
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
         
